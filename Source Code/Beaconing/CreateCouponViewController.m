7b3c02ee98d643363faab0be4ff92d0a6f3c449c
satz4ios <satz4ios@gmail.com>
2015-06-06 19:11:22 +0530

    Changed the create coupon UI and it's constraints

--- Source Code/Beaconing/CreateCouponViewController.m
+++ Source Code/Beaconing/CreateCouponViewController.m
@@ -23,6 +23,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.couponDescripFld.layer.borderWidth = 1.0f;
+    self.couponDescripFld.layer.borderColor = [[UIColor lightGrayColor] CGColor];
 }
 
 - (void)didReceiveMemoryWarning {
